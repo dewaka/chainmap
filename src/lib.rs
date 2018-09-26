6e50df3f9f6a3c9d9e89a98cdc5eb1953a52a7c3
Chathura Colombage <dcdewaka@gmail.com>
2018-09-26 21:28:28 +0100

    implement to_map

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ pub struct ChainMap<K, V> {
 }
 
 impl<K: Hash + Eq, V> ChainMap<K, V> {
+    /// Returns the first element found in the maps chain or None
     pub fn get(&self, k: &K) -> Option<&V> {
         // Check whether an element is found in any one of the maps
         for m in &self.maps {
@@ -20,6 +21,9 @@ impl<K: Hash + Eq, V> ChainMap<K, V> {
         None
     }
 
+    /// Inserts an element to the first map of the chain if maps are not empty,
+    /// and returns the current value in the case where the key exists, or
+    /// otherwise None
     pub fn insert(&mut self, k: K, v: V) -> Option<V> {
         if self.maps.len() == 0 {
             None
@@ -28,6 +32,7 @@ impl<K: Hash + Eq, V> ChainMap<K, V> {
         }
     }
 
+    /// Returns whether the ChainMap is empty or not
     pub fn is_empty(&self) -> bool {
         for m in &self.maps {
             if !m.is_empty() {
@@ -37,14 +42,17 @@ impl<K: Hash + Eq, V> ChainMap<K, V> {
         true
     }
 
+    /// Constructs a ChainMap with given vector of sub-maps
     pub fn new(maps: Vec<ChainMapType<K, V>>) -> Self {
         ChainMap { maps }
     }
 
+    /// Constructs an empty ChainMap
     pub fn empty() -> Self {
-        ChainMap { maps: vec![] }
+        ChainMap::new(vec![])
     }
 
+    /// Adds a map to the end of the maps chain
     pub fn add_map(&mut self, m: ChainMapType<K, V>) -> &mut Self {
         self.maps.push(m);
         self
@@ -52,6 +60,21 @@ impl<K: Hash + Eq, V> ChainMap<K, V> {
 }
 
 impl<K: Hash + Eq + Clone, V: Clone> ChainMap<K, V> {
+    /// Returns a single consolidated map
+    pub fn to_map(&self) -> ChainMapType<K, V> {
+        let mut combined_map = ChainMapType::new();
+
+        for m in &self.maps {
+            for (k, v) in m {
+                combined_map.insert(k.clone(), v.clone());
+            }
+        }
+
+        combined_map
+    }
+
+    /// Returns an optional ChainMap with just the end of maps removed, if the
+    /// current ChainMap is not empty. Otherwise returns None
     pub fn parents(&self) -> Option<Self> {
         if self.maps.len() > 0 {
             let (ps, _) = self.maps.split_at(self.maps.len() - 1);
@@ -61,6 +84,8 @@ impl<K: Hash + Eq + Clone, V: Clone> ChainMap<K, V> {
         }
     }
 
+    /// Returns an optional ChainMap with just head of maps removed, if the
+    /// current ChainMap is not empty. Otherwise returns None
     pub fn children(&self) -> Option<Self> {
         if self.maps.len() > 0 {
             let (_, cs) = self.maps.split_at(1);
@@ -194,5 +219,29 @@ mod tests {
 
         cmap.add_map(m);
         assert_eq!(cmap.get(&3), Some(&"three".to_string()));
+    }
+
+    #[test]
+    fn test_chainmap_to_map() {
+        let mut m1 = HashMap::<i32, String>::new();
+        m1.insert(1, "one".to_string());
+        m1.insert(2, "two".to_string());
+        m1.insert(3, "three".to_string());
+
+        let mut m2 = HashMap::<i32, String>::new();
+        m2.insert(11, "eleven".to_string());
+        m2.insert(22, "twenty two".to_string());
+        m2.insert(33, "thirty three".to_string());
+
+        let cmap = ChainMap::new(vec![m1, m2]);
+
+        let combined_map = cmap.to_map();
+
+        assert_eq!(combined_map.get(&1), Some(&"one".to_string()));
+        assert_eq!(combined_map.get(&2), Some(&"two".to_string()));
+        assert_eq!(combined_map.get(&3), Some(&"three".to_string()));
+        assert_eq!(combined_map.get(&11), Some(&"eleven".to_string()));
+        assert_eq!(combined_map.get(&22), Some(&"twenty two".to_string()));
+        assert_eq!(combined_map.get(&33), Some(&"thirty three".to_string()));
     }
 }
